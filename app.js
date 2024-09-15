e4bfefe1fcc95a5066c4d763a6a0b7bb0e7f468f
13alinnn <iliescualinus@gmail.com>
2024-09-16 01:22:14 +0300

    added functionality to keys

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const keys = document.querySelectorAll('.key');
+const numbers = document.querySelectorAll('.num');
 const del = document.querySelector('.delete');
 const clear = document.querySelector('.clear');
 const screen = document.querySelector('.screen p');
+
 keys.forEach(key => {
     const initialColor = key.style.backgroundColor;
     if(key.className.split(' ').includes('clear')) {
@@ -31,12 +33,19 @@ keys.forEach(key => {
         }
     }
     });
-    clear.addEventListener('click', () => {
-        screen.textContent = '';
+
+numbers.forEach(number => {
+    number.addEventListener('click',() => {
+        const value = number.textContent;
+        screen.textContent += value;
     });
-    function deleteLastLetter(string) {
-        return string.slice(0,-1);
-    }
-    del.addEventListener('click', () => {
-        screen.textContent = deleteLastLetter(screen.textContent);
-    })
+});
+clear.addEventListener('click', () => {
+    screen.textContent = '';
+});
+function deleteLastLetter(string) {
+    return string.slice(0,-1);
+}
+del.addEventListener('click', () => {
+    screen.textContent = deleteLastLetter(screen.textContent);
+});
