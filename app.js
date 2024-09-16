f52d4324854e261b09f3968f1232c4ccdfe44d9c
13alinnn <iliescualinus@gmail.com>
2024-09-16 18:09:12 +0300

    deleted the input functions, starting to rewrite them

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const current = document.querySelector(".current");
 const last = document.querySelector(".last");
 const eq = document.querySelector(".eq");
 let result = 0;
+let input;
+let num1;
+let num2;
 let operation = '';
+let waitForInput = true;
+
 keys.forEach((key) => {
 	const initialColor = key.style.backgroundColor;
 	if (key.className.split(" ").includes("clear")) {
@@ -39,8 +44,16 @@ keys.forEach((key) => {
 numbers.forEach((number) => {
 	number.addEventListener("click", () => {
 		const value = number.textContent;
-		if (current.textContent !== "0") current.textContent += value;
-		else current.textContent = value;
+		if(waitForInput) {
+			current.textContent = value;
+			waitForInput = false;
+		}
+		else {
+			current.textContent += value;
+			num2 = parseFloat(current.textContent);
+			num2 = Math.round(num2 * 10) / 10;
+			current.textContent = num2;
+		}
 	});
 });
 
@@ -57,60 +70,29 @@ del.addEventListener("click", () => {
 	current.textContent = deleteLastLetter(current.textContent);
 });
 
-// input
-operators.forEach((operator) => {
-	operator.addEventListener("click", () => {
-		if (current.textContent) {
-			if (last.textContent === "") {
-				last.textContent = `${current.textContent} ${operator.textContent}`;
-				current.textContent = "0";
-			} else if (
-				(last.textContent.slice(-1) === "+" ||
-					last.textContent.slice(-1) === "-" ||
-					last.textContent.slice(-1) === "÷" ||
-					last.textContent.slice(-1) === "×") &&
-				current.textContent === "0"
-			) {
-				last.textContent = last.textContent.slice(0, -1);
-				last.textContent += operator.textContent;
-				current.textContent = "0";
-			} else {
-				last.textContent = `${current.textContent} ${operator.textContent}`;
-				current.textContent = "0";
-			}
-            //about to add feature where operators function like the equals button when pressed if last content has text inside it
-		}
-	});
-});
 
-eq.addEventListener('click', () => {
-	if((last.textContent || last.textContent === '0') && (current.textContent || current.textContent === '0')) {
-		equal();
-	}
-});
-
-function equal() {
-	let result;
-	let num1 = parseInt(last.textContent,10);
-	let num2 = parseInt(current.textContent);
-	operation = last.textContent.slice(-1);
-	switch(operation) {
-		case '×':
-			result = num1 * num2;
-			break;
-		case '÷':
-			if(num2 === 0)
-				result = 'ERROR';
-			else	
-				result = num1 / num2;
-			break;
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-	}
-	last.textContent = `${num1} ${operation} ${num2}`;
-	current.textContent = result;
-}
+// function equal() {
+// 	let result;
+// 	let num1 = parseInt(last.textContent,10);
+// 	let num2 = parseInt(current.textContent);
+// 	operation = last.textContent.slice(-1);
+// 	switch(operation) {
+// 		case '×':
+// 			result = num1 * num2;
+// 			break;
+// 		case '÷':
+// 			if(num2 === 0)
+// 				result = 'ERROR';
+// 			else	
+// 				result = num1 / num2;
+// 			break;
+// 		case '+':
+// 			result = num1 + num2;
+// 			break;
+// 		case '-':
+// 			result = num1 - num2;
+// 			break;
+// 	}
+// 	last.textContent = `${num1} ${operation} ${num2}`;
+// 	current.textContent = result;
+// }
