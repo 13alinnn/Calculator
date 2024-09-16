48ba43e1d8e0c01889a91232f364c4a913a0fecd
13alinnn <iliescualinus@gmail.com>
2024-09-16 20:34:00 +0300

    rewrote the input method, now works

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const keys = document.querySelectorAll(".key");
+const buttons = document.querySelectorAll(".key");
+const keys = document.querySelectorAll('.key:not(.delete):not(.clear):not(.eq)');
 const numbers = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".operator");
 const del = document.querySelector(".delete");
@@ -6,14 +7,15 @@ const clear = document.querySelector(".clear");
 const current = document.querySelector(".current");
 const last = document.querySelector(".last");
 const eq = document.querySelector(".eq");
-let result = 0;
-let input;
+let input ='';
 let num1;
 let num2;
 let operation = '';
-let waitForInput = true;
+let symbolPressed = false;
+let result;
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-keys.forEach((key) => {
+buttons.forEach((key) => {
 	const initialColor = key.style.backgroundColor;
 	if (key.className.split(" ").includes("clear")) {
 		key.addEventListener("mouseover", () => {
@@ -41,25 +43,69 @@ keys.forEach((key) => {
 	}
 });
 
-numbers.forEach((number) => {
-	number.addEventListener("click", () => {
-		const value = number.textContent;
-		if(waitForInput) {
-			current.textContent = value;
-			waitForInput = false;
+
+keys.forEach(key => {
+	key.addEventListener('click', () => {
+		console.log(key.textContent);
+		if(symbolPressed === false) {
+			if(key.textContent === '+' || key.textContent === '-' || key.textContent === '*' || key.textContent === '/') {
+				symbolPressed = true;
+			}
+			input += key.textContent;
 		}
 		else {
-			current.textContent += value;
-			num2 = parseFloat(current.textContent);
-			num2 = Math.round(num2 * 10) / 10;
-			current.textContent = num2;
+			if(!(key.textContent === '+' || key.textContent === '-' || key.textContent === '*' || key.textContent === '/')) {
+				input += key.textContent;
+			}
+			else {
+				//stores the first number in num1, the operator in operation and the second number in num2
+				// symbolPressed = false;
+				let index = -1;
+				for(let i = 0; i < input.length; i++) {
+					if('+-/*'.includes(input[i])) {
+						index = i;
+						break;
+					}
+				}
+				num1 = parseFloat(input.slice(0,index));
+				operation = input[index];
+				num2 = parseFloat(input.slice(index + 1));
+				result = operate(num1, operation, num2);
+				input = result;
+				if(input === 'ERROR') {
+					setTimeout(() => {
+						current.textContent = "0";
+						last.textContent = "";
+						input = '';
+					},3000);
+				}
+				input += key.textContent;
+				current.textContent = input;
 		}
+	}
+		current.textContent = input;
 	});
 });
+
+
+function operate(num1, operation, num2) {
+	switch(operation) {
+		case '*':
+			return num1 * num2;
+		case '/':
+			if(num2 === 0) return 'ERROR';
+			return num1 / num2;
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+	}
+}
 
 clear.addEventListener("click", () => {
 	current.textContent = "0";
 	last.textContent = "";
+	input = '';
 });
 
 function deleteLastLetter(string) {
@@ -68,31 +114,31 @@ function deleteLastLetter(string) {
 
 del.addEventListener("click", () => {
 	current.textContent = deleteLastLetter(current.textContent);
+	input = deleteLastLetter(input);
 });
 
-
-// function equal() {
-// 	let result;
-// 	let num1 = parseInt(last.textContent,10);
-// 	let num2 = parseInt(current.textContent);
-// 	operation = last.textContent.slice(-1);
-// 	switch(operation) {
-// 		case '×':
-// 			result = num1 * num2;
-// 			break;
-// 		case '÷':
-// 			if(num2 === 0)
-// 				result = 'ERROR';
-// 			else	
-// 				result = num1 / num2;
-// 			break;
-// 		case '+':
-// 			result = num1 + num2;
-// 			break;
-// 		case '-':
-// 			result = num1 - num2;
-// 			break;
-// 	}
-// 	last.textContent = `${num1} ${operation} ${num2}`;
-// 	current.textContent = result;
-// }
+eq.addEventListener('click', () => {
+	let index = -1;
+	for(let i = 0; i < input.length; i++) {
+		if('+-/*'.includes(input[i])) {
+			index = i;
+			break;
+		}
+	}
+	if(index !== -1) {
+		num1 = parseFloat(input.slice(0,index));
+		operation = input[index];
+		num2 = parseFloat(input.slice(index + 1));
+		result = operate(num1, operation, num2);
+		input = result;
+		if(input === 'ERROR') {
+			setTimeout(() => {
+				current.textContent = "0";
+				last.textContent = "";
+				input = '';
+			},3000);
+		}
+		current.textContent = input;
+		symbolPressed = false;
+	}
+});
